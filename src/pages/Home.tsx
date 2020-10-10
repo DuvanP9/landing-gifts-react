83fb1d4b24739c33799a38012acb99997b38941a
DuvanP9 <dpedroza@dbsolution.com.co>
2020-10-10 07:56:37 -0500

    install-react-router-dom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
+    <div>
       <h1>project creation</h1>    
     </div>
   );
 }
 
-export default App;
+export default Home;
