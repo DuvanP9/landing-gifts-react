348f1a8906b202449c1600411aef614c0b7d0abc
DuvanP9 <dpedroza@dbsolution.com.co>
2020-10-12 18:24:31 -0500

    logout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import { Route, Redirect } from "react-router-dom";
+import { useHistory, Route, Redirect } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import useStyles from './styles/Home'
 
 function Home() {
-  
+
+  const history = useHistory();
+  const classes = useStyles();
   const isAuthed = !!localStorage.getItem('auth');
+
+  
+const handleLogout = () => {
+  localStorage.clear();
+  history.push('/login');
+};
 
   return (
     <Route exact path="/">
-      {isAuthed ? <Redirect to="/landing" /> : <Redirect to="/login" /> }
+      {isAuthed ? 
+      <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <div className={classes.logout}>
+
+            <Button variant="contained" color="default" onClick={handleLogout}>
+              Cerrar Sesion
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment> 
+      : <Redirect to="/login" /> }
     </Route>
   );
 }
---
+++ src/pages/styles/Home.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    logout: {
+      position: 'absolute',
+      right: 10
+    },
+  }));
+
+  export default useStyles;
