b34a56183b660453318d15b3bb9921776e98fc33
DuvanP9 <dpedroza@dbsolution.com.co>
2020-10-12 15:44:33 -0500

    visual-components-singup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 
 import Login from '../pages/Login';
+import SignUp from '../pages/register';
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
           <Route exact path="/login" component={Login} />
+          <Route exact path="/singup" component={SignUp} />
           <Route exact path="/" component={Home} />
         </Switch>
     </BrowserRouter>
